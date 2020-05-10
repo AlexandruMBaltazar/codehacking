65c00337201f00da74e092fc64673e96dfa259c2
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2020-05-10 15:25:12 +0100

    Admin users page is fully functional

--- app/User.php
+++ app/User.php
@@ -25,8 +25,6 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-
-
     public function role(){
         return $this->belongsTo('App\Role');
     }
@@ -35,5 +33,13 @@ class User extends Authenticatable
         return $this->belongsTo('App\Photo');
     }
 
+    public function isAdmin(){
+
+        if($this->role->name == "administrator" && $this->is_active == 1){
+            return true;
+        }
+
+        return false;
+    }
 
 }
