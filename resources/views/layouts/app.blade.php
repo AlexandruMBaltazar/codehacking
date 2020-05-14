5ee273c50c3fa6463c4bb3b99d407c6aeeb82e09
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2020-05-14 15:15:26 +0100

    able to create posts with dynamic categories

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,9 @@
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Home</a></li>
+                    @if(\Illuminate\Support\Facades\Auth::check() && \Illuminate\Support\Facades\Auth::user()->isAdmin())
+                        <li><a href="{{ url('/admin') }}">Dashboard</a></li>
+                    @endif
                 </ul>
 
                 <!-- Right Side Of Navbar -->
