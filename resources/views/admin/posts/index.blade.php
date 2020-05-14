237433fe351edcce89db84fa21c13a74e421c834
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2020-05-14 14:28:08 +0100

    Able to create post with static category

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Posts</h1>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Owner</th>
+            <th>Category</th>
+            <th>Photo</th>
+            <th>Title</th>
+            <th>Body</th>
+            <th>Created</th>
+            <th>Updated</th>
+        </tr>
+        </thead>
+        <tbody>
+        @if($posts)
+            @foreach($posts as $post)
+                <tr>
+                    <td>{{$post->id}}</td>
+                    <td>{{$post->user->name}}</td>
+                    <td>{{$post->category_id}}</td>
+                    <td>
+                        @if($post->photo)
+                            <img height="50" src="{{$post->photo->getProfilePic($post->photo->file)}}">
+                        @else
+                            <img height="50" src="{{asset('images/placeholder.png')}}">
+                        @endif
+                    </td>
+                    <td>{{$post->title}}</td>
+                    <td>{{$post->body}}</td>
+                    <td>{{$post->created_at->diffForhumans()}}</td>
+                    <td>{{$post->updated_at->diffForhumans()}}</td>
+                </tr>
+            @endforeach
+        @endif
+        </tbody>
+    </table>
+@stop
